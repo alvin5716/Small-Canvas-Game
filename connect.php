cd681ce406d9df7586386fc9c2786afa0e8d5d75
travischen0219 <35632139+travischen0219@users.noreply.github.com>
2018-06-24 14:24:45 +0800

    Gfriend0624
    new login window and background-image

---
+++ connect.php
@@ -0,0 +1,19 @@
+﻿<?php session_start(); ?>
+<!--上方語法為啟用session，此語法要放在網頁最前方-->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<?php
+
+$id=$_POST['id'];
+$pw=$_POST['pw'];
+
+if($id != null && $pw != null && $id=='user' && $pw=='181818')
+{
+        echo '登入成功!';
+        echo '<meta http-equiv=REFRESH CONTENT=1;url=canvas.html>';
+}
+else
+{
+        echo '登入失敗!';
+        echo '<meta http-equiv=REFRESH CONTENT=1;url=index.php>';
+}
+?>
---
+++ index.php
@@ -0,0 +1,18 @@
+﻿<!-- 設定網頁編碼為UTF-8 -->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<form name="form" method="post" action="connect.php">
+ 
+<body style="background-image: url(bg.jpg); background-repeat: no-repeat; height: auto; /* Center and scale the image nicely */ background-position: center; background-repeat: no-repeat; background-size: cover; ; margin: 0px auto; padding-top: 10%; background-color: #00EBF9; color: #FFF; font: 'Microsoft JhengHei'; padding-left: 40%;">
+    
+
+<div>
+帳號：<input style="font-family:'Microsoft JhengHei'; font-size:24px" type="text" name="id" />
+<br>
+密碼：<input style="font-family:'Microsoft JhengHei'; font-size:24px" type="password" name="pw" />
+<input style="font-family: 'Microsoft JhengHei'; font-size: 16px; font: 'Microsoft JhengHei'; height: auto;" type="submit" name="button" value="點我登入" />
+<br>
+</div>
+
+
+</body>
+</form>
